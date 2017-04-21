e9700c6f40827b74f9461a72ccd370f605e38c08
Luis Colorado <luiscoloradourcola@gmail.com>
2017-04-22 02:02:21 +0300

    tested
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

--- mcm/main.cc
+++ mcm/main.cc
@@ -4,20 +4,34 @@
  */
 
 #include "mcm.h"
+#include <cstdlib>
 
 int main()
 {
-    MCM::mc_matrix<double> mat(15);
+    MCM::mc_matrix<int> mat(8);
+    srand(time(0));
+
     for (int row = 0; row < mat.dim; row++)
         for (int col = 0; col < mat.dim; col++)
-            mat[row][col] = !row || !col ? 1 : mat[row-1][col] + mat[row][col-1];
+            mat[row][col] = random() % 100; //!row || !col ? 1 : mat[row-1][col] + mat[row][col-1];
+
     std::cout << mat << std::endl;
-    MCM::mc_node<double>* n1 = new MCM::mc_node<double>(mat, mat.getRoot(), 3);
-    MCM::mc_node<double>* n2 = new MCM::mc_node<double>(mat, n1, 2);
-    std::cout << "n1 = " << *n1 << std::endl;
-    std::cout << "n2 = " << *n2 << std::endl;
-    std::cout << "TREE:\n";
+
+    const MCM::mc_node<int>* res;
+    int n = 1;
+
+    do {
+        res = mat.get_mcm();
+        n++;
+        //std::cout << "candidate: " << *res << std::endl;
+    } while (res && res->col != mat.dim - 1);
+
     mat.getRoot()->print(std::cout);
-    mat.reset();
-    mat.getRoot()->print(std::cout);
+
+    std::cout << mat << std::endl;
+    std::cout << "SOLUTION: " << *res << std::endl;
+    std::cout << "REVISED: " << n << " posiciones\n";
+    std::cout << "FRONTIER: " << mat.frontier->size() << std::endl;
+
+    return 0;
 }
--- mcm/mcm.h
+++ mcm/mcm.h
@@ -15,6 +15,8 @@ namespace MCM {
     template <typename T>
     class sq_matrix {
 
+    protected:
+
         T **const array; 
 
     public:
@@ -44,34 +46,29 @@ namespace MCM {
 
     public:
 
-        sq_matrix<T>&       matrix;
-        mc_node<T>*const    parent;
-        const int           row, col;
-        const T             cost;
-        mc_node<T>**const   children;
+        const sq_matrix<T>&         matrix;
+        const mc_node<T>*const      parent;
+        const int                   row, col;
+        const T                     cost;
+        const mc_node<T>**const     children;
 
-        mc_node(sq_matrix<T>& matrix,
-                mc_node<T> * const parent,
-                int row):
-
-                matrix(matrix),
-                parent(parent),
-                row(row),
+        mc_node(const sq_matrix<T>& matrix, const mc_node<T>* parent = 0, const int row = -1):
+                matrix(matrix), parent(parent), row(row),
                 col(parent
                         ? parent->col+1
                         : -1),
                 cost(parent
                         ? parent->cost + matrix[row][col]
                         : T()),
-                children(new mc_node<T>*[matrix.dim])
+                children(new const mc_node<T>*[matrix.dim])
         {
-            std::cout << this << ": " << __func__ << "(matrix, " << parent << ", " << row << ");\n";
+            //std::cout << this << ": " << __func__ << "(matrix, " << parent << ", " << row << ");\n";
             for (int i = 0; i < matrix.dim; i++)
                 children[i] = 0;
-            std::cout << this << ": creating " << matrix.dim << " children\n";
+            //std::cout << this << ": creating [" << matrix.dim << "] children\n";
             if (parent) {
                 assert(parent->children[row] == 0);
-                std::cout << this << ": adding this to parent's children\n";
+                //std::cout << this << ": adding 'this' to parent's children\n";
                 parent->children[row] = this;
             }
         }
@@ -81,36 +78,46 @@ namespace MCM {
                 if (children[i]) delete children[i];
             delete [] children;
 
-            std::cout << "deleting " << this;
-            if (parent) std::cout << ", child of " << parent;
-            std::cout << std::endl;
+            if (parent) parent->children[row] = 0;
+
+            //std::cout << "deleting " << this;
+            //if (parent) std::cout << ", child of " << parent;
+            //std::cout << std::endl;
         }
-        void print(std::ostream& out, const int indent = 0);
+        void print(std::ostream& out, const int indent = 0) const;
     };
 
     template <typename T>
     class mc_matrix: public sq_matrix<T> {
 
-        std::vector<mc_node<T>*> frontier;
-        mc_node<T> * root;
-
     public:
+        std::vector<const mc_node<T>*>* frontier;
+        const mc_node<T> root;
 
 
-        mc_matrix(int dim):sq_matrix<T>(dim) {
-            std::cout << this << ": " << __func__ << "(" << dim << ");\n";
-            frontier.push_back(root = new mc_node<T>(*this, 0, -1));
+        mc_matrix(int dim):sq_matrix<T>(dim),frontier(new std::vector<const mc_node<T>*>()), root(*this) {
+            //std::cout << this << ": " << __func__ << "(" << dim << ");\n";
+            frontier->push_back(&root);
         }
 
-        ~mc_matrix() { delete root; }
+        ~mc_matrix() {
+            delete frontier;
+        }
 
         void reset() {
-            frontier.clear();
-            delete root;
-            frontier.push_back(root = new mc_node<T>(*this, 0, -1));
+            for (int i = 0; i < this->dim; i++) {
+                if (root.children[i]) {
+                    delete root.children[i];
+                    root.children[i] = 0;
+                }
+            }
+            frontier->clear();
+            frontier->push_back(&root);
         }
 
-        mc_node<T> *const getRoot() { return root; }
+        const mc_node<T>* get_mcm();
+
+        const mc_node<T>* getRoot() { return &root; }
 
     };
 
@@ -136,17 +143,63 @@ std::ostream& operator<<(std::ostream& out, const MCM::sq_matrix<T>& it)
 template <typename T>
 std::ostream& operator<<(std::ostream& out, const MCM::mc_node<T>& node)
 {
-    if (node.parent == 0) return out << "@";
-    return out << *node.parent << "=>" << node.row << "(v=" << node.matrix[node.row][node.col] <<";c=" << node.cost << ")";
+    if (node.parent == 0) return out << "ROOT(c=0)";
+    return out << *node.parent << "=> [" << node.row << "](v=" << node.matrix[node.row][node.col] <<";c=" << node.cost << ")";
 }
 
 template <typename T>
-void MCM::mc_node<T>::print(std::ostream& out, const int indent)
+void MCM::mc_node<T>::print(std::ostream& out, const int indent) const
 {
     for (int i = 0; i < indent; i++) out << " ";
     out << *this << std::endl;
     for (int i = 0; i < matrix.dim; i++)
         if (children[i]) children[i]->print(out, indent + 1);
+}
+
+template <typename T>
+const MCM::mc_node<T>* MCM::mc_matrix<T>::get_mcm()
+{
+    std::vector<const MCM::mc_node<T>*>* new_frontier = new std::vector<const MCM::mc_node<T>*>();
+    const MCM::mc_node<T>* new_parent = 0;
+    int new_row;
+    T new_cost;
+
+    //std::cout << this << ": " << __func__ << "(): BEGIN\n";
+
+    for(typename std::vector<const MCM::mc_node<T>*>::iterator it = frontier->begin(); it != frontier->end(); it++) {
+        const MCM::mc_node<T>* candidate_parent = *it;
+
+        //std::cout << "Processing " << *candidate_parent << std::endl;
+        int candidates = 0;
+        for(int row = 0; row < this->dim; row++) {
+            const MCM::mc_node<T>* n;
+            for(n = candidate_parent; n->parent; n = n->parent)
+                if (n->row == row) // row coincides with some ancestor's
+                    break;
+            if (n->parent // we broke the loop (row found in ancestors chain)
+                    || candidate_parent->children[row]) // row is the same as children[row] (already visited)
+                continue;
+            candidates++;
+            int col = candidate_parent->col+1;
+            T cost = candidate_parent->cost + this->array[row][col];
+            if (!new_parent || cost < new_cost) {
+                // we found a candidate.  Create it.
+                new_parent = candidate_parent;
+                new_row = row;
+                new_cost = cost;
+            }
+        }
+        if (candidates)
+            new_frontier->push_back(candidate_parent);
+    }
+    delete frontier;
+    frontier = new_frontier;
+    if (new_parent) {
+        const MCM::mc_node<T>* result = new MCM::mc_node<T>(*this, new_parent, new_row);
+        frontier->push_back(result);
+        return result;
+    }
+    return 0;
 }
 
 #endif /* _MCM_H */
